6583089ad9634117dcbad0f0fbd1a6e8ca0a84b1
Totoum Mandoum Patricia <patricia.totoummandoum.etu@b13p24.fil.univ-lille.fr>
2025-01-21 16:21:19 +0100

    Le premier scénario devrait passer au vert !

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,45 @@
 // Tests unitaires
-
+import { InMemoryWebinarRepository } from 'src/webinars/adapters/webinar-repository.in-memory';
+import { ChangeSeats } from 'src/webinars/use-cases/change-seats';
+import { testUser } from 'src/users/tests/user-seeds';
+import { Webinar } from 'src/webinars/entities/webinar.entity';
+import { WebinarNotFoundException } from 'src/webinars/exceptions/webinar-not-found';
+import { WebinarNotOrganizerException } from 'src/webinars/exceptions/webinar-not-organizer';
+import { WebinarReduceSeatsException } from 'src/webinars/exceptions/webinar-reduce-seats';
+import { WebinarTooManySeatsException } from 'src/webinars/exceptions/webinar-too-many-seats';
 
 describe('Feature : Change seats', () => {
-  // Initialisation de nos tests, boilerplates...
+  let webinarRepository: InMemoryWebinarRepository;
+  let useCase: ChangeSeats;
+
+  const webinar = new Webinar({
+    id: 'webinar-id',
+    organizerId: testUser.alice.props.id,
+    title: 'Webinar title',
+    startDate: new Date('2024-01-01T00:00:00Z'),
+    endDate: new Date('2024-01-01T01:00:00Z'),
+    seats: 100,
+  });
+
+  beforeEach(() => {
+    webinarRepository = new InMemoryWebinarRepository([webinar]);
+    useCase = new ChangeSeats(webinarRepository);
+  });
+
   describe('Scenario: Happy path', () => {
-    // Code commun à notre scénario : payload...
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+
     it('should change the number of seats for a webinar', async () => {
-     // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+      // ACT
+      await useCase.execute(payload);
+
+      // ASSERT
+      const updatedWebinar = await webinarRepository.findById('webinar-id');
+      expect(updatedWebinar?.props.seats).toEqual(200);
     });
   });
 });
